41d6efc5e33e09747fc01313c1ca9869e6523548
SarahAwad24 <131468966+SarahAwad24@users.noreply.github.com>
2024-02-26 16:15:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,61 @@ import torch as torch
 import numpy as np
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av as av
+import logging
+import queue
+from pathlib import Path
+from typing import List, NamedTuple, Tuple
+import cv2
+import torch
 
-webrtc_streamer(key="sample")
+
+classes = [
+    "Apple",
+    "Banana",
+    "Beetroot",
+    "Bitter_Gourd",
+    "Bottle_Gourd",
+    "Cabbage",
+    "Capsicum",
+    "Carrot",
+    "Cauliflower",
+    "Cherry",
+    "Chilli",
+    "Coconut",
+    "Cucumber",
+    "EggPlant",
+    "Ginger",
+    "Grape",
+    "Green_Orange",
+    "Kiwi",
+    "Maize",
+    "Mango",
+    "Melon",
+    "Okra",
+    "Onion",
+    "Orange",
+    "Peach",
+    "Pear",
+    "Peas",
+    "Pineapple",
+    "Pomegranate",
+    "Potato",
+    "Radish",
+    "Strawberry",
+    "Tomato",
+    "Turnip",
+    "Watermelon",
+    "almond",
+    "walnut"
+]
+
+class Detection(NamedTuple):
+    class_name: str
+    label: str
+    score: float
+    box: np.ndrray
+
+    def generate_label_colors():
+        return np.random.uniform(0, 255, size=(len(classes), 3))
+
+    COLORS = generate_label_colors()
